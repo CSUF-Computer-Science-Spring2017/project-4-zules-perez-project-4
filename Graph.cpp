229639d1a20b25b7140f06b3e2bcfab97a3eb9b8
zperez00 <rancid80s@csu.fullerton.edu>
2017-05-09 02:24:03 -0700

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -32,10 +32,38 @@ void Graph::addEdge(Vertex i, Vertex j)
 }
 
 // return a vector of vertices adjacent to vertex n
-vector<Vertex> Graph::getAdjacentVertices(Vertex n)
+vector<Vertex> Graph::getAdjacentVertices(Vertex N)
 {
-	vector<Vertex> V;
-	return V;
+	vector<Vertex> vertices;
+	vector<vector<bool>>::iterator row;
+	vector<bool>::iterator col;
+
+	if (N == 0)
+	{
+		for (row = edges.begin(); row != edges.end(); row++)
+		{
+			if (row->at(N) == true)
+			{
+				vertices.push_back(row->at(N));
+			}
+
+		}
+		return vertices;
+	}
+	else
+	{
+		for (row = edges.begin(); row != edges.end(); row++)
+		{
+			for (col = row->begin(); col != row->end(); col++)
+			{
+				if (row->at(N) == true)
+				{
+					vertices.push_back(row->at(N).operator=(col.operator*()));
+				}
+			}
+		}
+		return vertices;
+	}
 }
 
 // return a list of names that contain friends of friends of person
