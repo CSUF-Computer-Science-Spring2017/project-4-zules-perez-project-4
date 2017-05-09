b698cd789c7c955eb473696ec7a5b3f9b6bb9950
zperez00 <rancid80s@csu.fullerton.edu>
2017-05-09 00:05:39 -0700

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -32,23 +32,29 @@ void Graph::addEdge(Vertex i, Vertex j)
 }
 
 // return a vector of vertices adjacent to vertex n
-vector<Vertex> Graph::getAdjacentVertices(Vertex n) {
+vector<Vertex> Graph::getAdjacentVertices(Vertex n)
+{
+	
 }
 
 // return a list of names that contain friends of friends of person
 // names should not be repeated
-vector<string> recommendFriends(Graph &graph, const string &person) {
+vector<string> recommendFriends(Graph &graph, const string &person) 
+{
+	
 }
 
 // read from a text file, the labels (names) to be associate with each vertex (int)
-void readNamesFromFile(Graph &graph, const string& filename) {
+void readNamesFromFile(Graph &graph, const string& filename) 
+{
 	ifstream inputFile;
 	inputFile.open(filename);
 	if (!inputFile)
 		throw invalid_argument("Unable to open file " + filename);
 	string s;
 	int i;
-	while (!inputFile.eof()) {
+	while (!inputFile.eof()) 
+	{
 		inputFile >> i >> s;
 		// cout << "adding label " << i << ": " << s << endl;
 		graph.addLabel(i, s);
@@ -56,18 +62,21 @@ void readNamesFromFile(Graph &graph, const string& filename) {
 }
 
 // read from a text file, the list of friends for each vertex (int)
-void readFriendsFromFile(Graph &graph, const string& filename) {
+void readFriendsFromFile(Graph &graph, const string& filename) 
+{
 	ifstream inputFile;
 	inputFile.open(filename);
 	if (!inputFile)
 		throw invalid_argument("Unable to open file " + filename);
 
-	for (string line; getline(inputFile, line); ) {
+	for (string line; getline(inputFile, line); ) 
+	{
 		istringstream iss(line);
 		int i;
 		iss >> i;
 		int j;
-		while (!iss.eof()) {
+		while (!iss.eof()) 
+		{
 			iss >> j;
 			// cout << "adding edge " << i << " to " << j << endl;
 			graph.addEdge(i, j);
