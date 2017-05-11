9fb73eb7a7015941ba3a47ac0b8a8fb2449b1748
zperez00 <rancid80s@csu.fullerton.edu>
2017-05-10 23:58:22 -0700

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,36 +1,43 @@
 #pragma once
+#include "stdafx.h"
+#include <map>
 #include <vector>
 #include <string>
-#include <map>
 
-typedef int Vertex;
+
 using namespace std;
 
-class Graph 
-{
+typedef int Vertex;
+
+class Graph{
 public:
-	Graph(int n);
-	int size();
-	void addLabel(Vertex i, string s); 
+	Graph(int n); // TO DO
+	int size(); // TO DO
+	void addLabel(Vertex i, string s);
 	void addEdge(Vertex i, Vertex j);
 	vector<Vertex> getAdjacentVertices(Vertex N);
-
+	string getLabel(Vertex N);
 	Vertex getVertex(string label);
-	string getLabel(Vertex n);
+
 
 private:
+	// TO DO
 	// member variables and functions to implement the public member functions
-	vector<vector<bool>> edges;
-	map<string,Vertex> labelsToInt;
-	map<Vertex,string> intToLabels;
-	int n; //number of vertices
+	vector< vector<Vertex>> adj;
+	map<string, Vertex> labelsToInt;
+	map<Vertex, string> intToLabels;
+	int n;
 };
+
+// TO DO
 // return a list of names that contain friends of friends of person
 // names should not be repeated
 vector<string> recommendFriends(Graph &graph, const string& person);
 
+// COMPLETED
 // read from a text file, the labels (names) to be associate with each vertex (int)
 void readNamesFromFile(Graph &graph, const string& filename);
 
+// COMPLETED
 // read from a text file, the list of friends for each vertex (int)
 void readFriendsFromFile(Graph &graph, const string& filename);
