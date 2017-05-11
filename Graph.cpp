bd13d54bc8c8515f58d5bb7c67e0422606555a89
zperez00 <rancid80s@csu.fullerton.edu>
2017-05-10 23:57:46 -0700

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -1,78 +1,98 @@
+#include "stdafx.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>  // for std::istringstream
 #include "Graph.h"
 
+// TO DO
 // initialize an undirected graph that can store at most n vertices
-Graph::Graph(int n) 
+Graph::Graph(int n)
 {
-	this->n = n;
-	edges.resize(n, vector<bool>(n,false));
-	labelsToInt[string()] = n;
-	intToLabels[n] = string();
-}
-// return the maximum number of vertices
-int Graph::size() 
-{
-	return n;
+	
+	adj.resize(n);
 }
 
+// TO DO
+// return the maximum number of vertices
+int Graph::size()
+{
+	return adj.size();
+}
+
+// TO DO
 // give a string label to vertex
 void Graph::addLabel(Vertex i, string s) 
 {
-	labelsToInt[s] = i;
 	intToLabels[i] = s;
+	labelsToInt[s] = i;
 }
 
+// TO DO
 // add an edge between vertices i and j
-void Graph::addEdge(Vertex i, Vertex j)
+void Graph::addEdge(Vertex i, Vertex j) 
 {
-	edges[i][j] = true;
+	adj[i].push_back(j);
 }
 
 // return a vector of vertices adjacent to vertex n
-vector<Vertex> Graph::getAdjacentVertices(Vertex N)
+vector<Vertex> Graph::getAdjacentVertices(Vertex n)
 {
-	vector<Vertex> vertices;
-	vector<vector<bool>>::iterator row;
-	vector<bool>::iterator col;
-
-	if (N == 0)
+	vector<Vertex> vertex_adj;
+	//getting friends
+	for (int i = 0; i < adj[n].size(); i++) 
 	{
-		for (row = edges.begin(); row != edges.end(); row++)
-		{
-			if (row->at(N) == true)
-			{
-				vertices.push_back(row->at(N));
-			}
+		vertex_adj.push_back(adj[n][i]);
+	}
 
-		}
-		return vertices;
-	}
-	else
-	{
-		for (row = edges.begin(); row != edges.end(); row++)
-		{
-			for (col = row->begin(); col != row->end(); col++)
-			{
-				if (row->at(N) == true)
-				{
-					vertices.push_back(row->at(N).operator=(col.operator*()));
-				}
-			}
-		}
-		return vertices;
-	}
+	return vertex_adj;
 }
-
+// TO DO
 // return a list of names that contain friends of friends of person
 // names should not be repeated
 vector<string> recommendFriends(Graph &graph, const string &person) 
 {
-	vector<string> v;
-	return v;
+	//getting index of the person
+	Vertex index = graph.getVertex(person);
+	//getting friends of that person
+	vector<Vertex> vertex_adj = graph.getAdjacentVertices(index);
+
+	vector<string> recommended_friends;
+
+	vector<Vertex> vertex_recommend;
+	map<string, bool> unique_list;
+	string friend_name;
+
+	//adding himself to the unique list
+	friend_name = graph.getLabel(index);
+	unique_list[friend_name] = true;
+
+	//adding friends to unique list
+	for (int i = 0; i < vertex_adj.size(); i++) 
+	{
+		friend_name = graph.getLabel(vertex_adj[i]);
+		unique_list[friend_name] = true;
+
+	}
+
+	for (int i = 0; i<vertex_adj.size(); i++) {
+		//getting friends of friends
+		vertex_recommend = graph.getAdjacentVertices(vertex_adj[i]);
+
+		for (int j = 0; j < vertex_recommend.size(); j++) {
+
+			friend_name = graph.getLabel(vertex_recommend[j]);
+			//checking if not already added
+			if (!unique_list[friend_name]) {
+				recommended_friends.push_back(friend_name);
+				unique_list[friend_name] = true;
+			}
+		}
+	}
+	return recommended_friends;
 }
 
+
+// COMPLETED
 // read from a text file, the labels (names) to be associate with each vertex (int)
 void readNamesFromFile(Graph &graph, const string& filename) 
 {
@@ -82,14 +102,14 @@ void readNamesFromFile(Graph &graph, const string& filename)
 		throw invalid_argument("Unable to open file " + filename);
 	string s;
 	int i;
-	while (!inputFile.eof()) 
-	{
+	while (!inputFile.eof()) {
 		inputFile >> i >> s;
 		// cout << "adding label " << i << ": " << s << endl;
 		graph.addLabel(i, s);
 	}
 }
 
+// COMPLETED
 // read from a text file, the list of friends for each vertex (int)
 void readFriendsFromFile(Graph &graph, const string& filename) 
 {
@@ -98,14 +118,12 @@ void readFriendsFromFile(Graph &graph, const string& filename)
 	if (!inputFile)
 		throw invalid_argument("Unable to open file " + filename);
 
-	for (string line; getline(inputFile, line); ) 
-	{
+	for (string line; getline(inputFile, line);) {
 		istringstream iss(line);
 		int i;
 		iss >> i;
 		int j;
-		while (!iss.eof()) 
-		{
+		while (!iss.eof()) {
 			iss >> j;
 			// cout << "adding edge " << i << " to " << j << endl;
 			graph.addEdge(i, j);
@@ -113,18 +131,12 @@ void readFriendsFromFile(Graph &graph, const string& filename)
 	}
 }
 
-string Graph::getLabel(Vertex N)
+string Graph::getLabel(Vertex n)
 {
-	if (intToLabels.find(N) != intToLabels.end())
-		return intToLabels.find(N)->second;
-	else
-		return NULL;
+	return intToLabels[n];
 }
 
 Vertex Graph::getVertex(string label)
 {
-	if (labelsToInt.find(label) != labelsToInt.end())
-		return labelsToInt.find(label)->second;
-	else
-		return NULL;
+	return labelsToInt[label];
 }
